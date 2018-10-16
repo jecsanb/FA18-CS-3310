dc157bc3bbb3e28f9313f68c0506207b5863cdb8
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-10-15 23:09:06 -0500

    Assignment 5 skell added.

---
+++ C/Assignment5 /main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+/**
+ *  Jecsan Blanco
+ *  PROGRAMMING-LANGUAGES-CS-3310
+ *  Assignment 5
+ *
+ *  In this assignment you will store user input two square matrices of size n <= 10
+ *  as one dimensional arrays and compute the product of the first matrix times
+ *  the second matrix and store as another a single dimensional array and display
+ *  the product.
+ *
+ *  You can use only single dimension arrays for input and product matrices.
+ *
+ *  Your program must use malloc to allocate block of memory to store input
+ *  and product matrices as single dimensional arrays
+ *
+ * You program must compute product of the input matrices using a function such as:
+ * void mult(int * a, int *, int *b, int *c, int n), where matrix array c = matrix array
+ * a matrix array b, and n is size of the matrix.
+ * @return
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+/**
+ * Computes the producuct of the inputed matricies.
+ * @param a
+ * @param b
+ * @param c
+ * @param n
+ */
+void mult(int * a, int *, int *b, int *c, int n){
+
+}

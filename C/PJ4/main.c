6314fd214515876d970f7c9d7b36918eee20816d
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-10-26 21:56:47 -0500

    Program accept multiplication of positive and negative integers.

--- C/PJ4/main.c
+++ C/PJ4/main.c
@@ -1,3 +1,8 @@
+/**
+ * Jecsan Blanco
+ * 2018FA OP SYSTEMS & NETWRK (CS-3352-01)
+ * Project 4 - Partial manual input output.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,83 +10,113 @@
 #include <assert.h>
 #include <math.h>
 
-//returns the number of digits in n
 int countDigit(int n);
 
-int chartoint(char c);
+int charToInt(char c);
 
-int strtoint(char *str);
-void printstr(char *str);
+int strToInt(char *str);
 
-void printstr(char *str){
-    assert( str != NULL);
-    while(*str){
+void printStr(char *str);
+
+void printStr(char *str) {
+    assert(str != NULL);
+    while (*str) {
         putchar(*str);
         ++str;
     }
 }
+
+
 int countDigit(int n) {
-    n = abs(n);
+    if (n < 0) n = abs(n);
     return (int) (floor(log10(n))) + 1;
 }
 
-char  inttochar(int n) {
-    assert(n < 10);
+/**
+ *  Converts any digit [0-9] into its ascii representation.
+ */
+char inttochar(int n) {
+    assert(n >= 0 && n < 10);
     return (char) (n + 0x30);
-
 }
 
+
+/**
+ * A integer into its null terminated string representation.
+ */
 char *inttostr(int num) {
-    int digits = countDigit(num) + 1;
+    int sign = num < 0;
+    int digits = countDigit(num) + 1 + sign;
     char *string = malloc((digits) * sizeof(char));
+    // handle negatives
+    if (sign) {
+        string[0] = '-';
+        num = abs(num);
+    }
     int v = 0;
-    for (int i = digits-2; i >= 0; --i) {
+    for (int i = digits - 2; i >= 0 + sign; --i) {
         v = num % 10;
         string[i] = inttochar(v);
         num /= 10;
     }
-    string[digits -1 ] = 0;
+    string[digits - 1] = 0;
     return string;
 }
 
-//turns a char to its integer representation
-int chartoint(char c) {
+/**
+ * Turns a single ascii digit into its integer value.
+ */
+int charToInt(char c) {
+    assert(c >= '0' && c <= '9');
     return c & 0x0F;
 }
 
-// turns a null terminated string to its integer representation
-//e.g "12" -> 12 where typeof(12) = int
-int strtoint(char *str) {
+/**
+ * Turns a null terminated string of ascii digits to its integer representation.
+ */
+int strToInt(char *str) {
     assert(str != NULL);
+    int sign = (str[0] == '-') ? 1 : 0;
+    if (sign) ++str;
     int digits = (int) strlen(str);
+
     int result = 0;
     int magnitude = 1;
     for (int i = digits - 1; i >= 0; --i) {
-        result += chartoint(str[i]) * magnitude;
+        result += charToInt(str[i]) * magnitude;
         magnitude *= 10;
-    };
-    return result;
+    }
+    return (sign) ? result * -1 : result;
 }
 
-int main() {
-    int buffer_size = 256;
-    char *string = (char *) malloc(buffer_size * sizeof(char));
-    read(1, string, (size_t) 256);
+/**
+ * Asks for two integers separated by a space to be multiplied.
+ */
+int main(int argc, char *argv[]) {
+    char *input, *a, *b, *output;
+    char delimiter;
+    int result;
+    int buffer_size;
 
-    char delimiter = ' ';
-    //get the first number
-    char *a = strsep(&string, &delimiter);
-    //get the second number
-    char *b = string;
-    //remove the nl
+    delimiter = ' ';
+    buffer_size = 255;
+
+    input = (char *) malloc(buffer_size * sizeof(char));
+    read(1, input, (size_t) 256);
+    assert(input != NULL);
+
+    //first number and second numbers
+    b = input;
+    a = strsep(&input, &delimiter);
+    //removes the newline
     b[strcspn(b, "\n")] = 0;
 
-    // printf("A:%s size=%d,"
-    // "B:%s size=%d\n", a, achars, b, bchars);
-    // printf("valueA is: %d\n",strtoint(a));
-    // printf("valueB is: %d\n",strtoint(b));
-    int result = strtoint(a) * strtoint(b);
-    char *strresult = inttostr(result);
-    printstr(strresult);
+    //do the thing
+    result = strToInt(a) * strToInt(b);
+    output = inttostr(result);
+    printStr(output);
+    //Land of the
+    free(a);
+    free(output);
     return 0;
 }

547c560fa13f95d8771560387626370aee46429b
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-11-09 20:58:27 -0600

    lab started

---
+++ C/blanco_08/lab8.c
@@ -0,0 +1,12 @@
+/**
+ * Jecsan Blanco
+ * 2018FA PROGRAMMING LANGUAGES (CS-3310-01)
+ * Write a C Linux program to multiply two square matrices of size <= 10 using
+ * three different multiplication functions.
+ */
+
+#include <stdio.h>
+int main(int argc, char** argsv) {
+    printf("Hello, World!\n");
+    return 0;
+}
